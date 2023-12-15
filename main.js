9030ee71fe935eb218c08c310addfd75ff4677f1
jnovak986 <jnovak986@gmail.com>
2023-12-15 17:39:05 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+function hodKostkou() {
+    let kostkaValue = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("value_text").innerHTML = kostkaValue;
+}
+
+function hodDvemaKostkami() {
+    let kostkaValue = Math.floor(Math.random() * 6) + 1;
+    let kostka2Value = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("value_text").innerHTML = (kostkaValue + " " + kostka2Value)
+}
+
+function hodTremiKostkami() {
+    let kostkaValue = Math.floor(Math.random() * 6) + 1;
+    let kostka2Value = Math.floor(Math.random() * 6) + 1;
+    let kostka3Value = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("value_text").innerHTML = (kostkaValue + " " + kostka2Value + " " + kostka3Value)
+}
+
+function nekonecne_hazeni() {
+    hodKostkou();
+    setInterval(hodKostkou, 500);
+}
+
+function nekonecne_hazeni2() {
+    hodDvemaKostkami();
+    setInterval(hodDvemaKostkami, 500);
+}
+
+function nekonecne_hazeni3() {
+    hodTremiKostkami();
+    setInterval(hodTremiKostkami, 500);
+}
+
+function toggleCzech() { // Aktuálně rozpracováno a nevyužito
+    document.getElementById("page_title").innerHTML = "Házení kostek";
+    document.getElementById("1die").innerHTML = "1 kostka";
+    document.getElementById("2die").innerHTML = "2 kostky";
+    document.getElementById("3die").innerHTML = "3 kostky";
+    document.getElementById("infinite1").innerHTML = "Nekonecné házení 1 kostky";
+    document.getElementById("infinite2").innerHTML = "Nekonecné házení 2 kostek";
+    document.getElementById("infinite3").innerHTML = "Nekonecné házení 3 kostek";
+}
